77cccf36466f5bdca319673e658c64ca64cb0f9c
Keijo Kapp <keijo.kapp@gmail.com>
2025-03-12 15:01:56 +0200

    Removed custom build directory

--- build.sh
+++ build.sh
@@ -12,9 +12,3 @@ else
 fi
 
 stack build
-
-TARGET_DIR="$(readlink -f $(dirname $0))/.build"
-rm -rf $TARGET_DIR
-mkdir $TARGET_DIR
-cp "$(stack path --local-install-root)/bin/events-form" $TARGET_DIR
-cp -R templates www $TARGET_DIR
